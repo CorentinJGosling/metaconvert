b8e0cd1682c301439a299ad76f7913d7331bc2cf
CorentinJGosling <you@example.com>
2024-01-29 23:07:38 +0100

    29 01 24

--- script/input.js
+++ script/input.js
@@ -59,32 +59,32 @@ window.addEventListener("scroll", () => {
 //   }
 // });
 
-const Switch_table = document.querySelector(".esm");
-const checkbox = document.querySelector(".myCheckbox");
-Switch_table.style.opacity = 0;
-Switch_table.style.marginTop = "-20px";
+// const Switch_table = document.querySelector(".esm");
+// const checkbox = document.querySelector(".myCheckbox");
+// Switch_table.style.opacity = 0;
+// Switch_table.style.marginTop = "-20px";
 
-function go2(event) {
-  event.preventDefault();
-  Switch_table.animate(
-    {
-      opacity: [0, 1],
-      marginTop: ["-10px", 0],
-    },
-    {
-      duration: 500,
-      fill: "forwards",
-    }
-  );
-}
-checkbox.addEventListener("change", function (event) {
-  if (event.target.checked) {
-    Switch_table.classList.add("show_toc");
-  } else {
-    Switch_table.classList.remove("show_toc");
-  }
-});
-checkbox.addEventListener("change", go2);
+// function go2(event) {
+//   event.preventDefault();
+//   Switch_table.animate(
+//     {
+//       opacity: [0, 1],
+//       marginTop: ["-10px", 0],
+//     },
+//     {
+//       duration: 500,
+//       fill: "forwards",
+//     }
+//   );
+// }
+// checkbox.addEventListener("change", function (event) {
+//   if (event.target.checked) {
+//     Switch_table.classList.add("show_toc");
+//   } else {
+//     Switch_table.classList.remove("show_toc");
+//   }
+// });
+// checkbox.addEventListener("change", go2);
 
 //  ROUTING
 const seeEffectMeasure = document.querySelector(".btn_effect_measure");
@@ -104,9 +104,9 @@ const cards = document.querySelectorAll(".table-title");
 //   listInput.classList.add("show_toc");
 //   Tables.classList.add("show_toc");
 
-//   cards.forEach((card) => {
-//     card.classList.add("show");
-//   });
+cards.forEach((card) => {
+  card.classList.add("show");
+});
 // });
 // seeEffectMeasure.addEventListener("click", () => {
 //   seeEffectMeasure.classList.add("btn_active");
@@ -117,55 +117,55 @@ const cards = document.querySelectorAll(".table-title");
 // });
 
 // Trigger the seeListInput click event when the page loads
-document.addEventListener("DOMContentLoaded", () => {
-  seeListInput.click();
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   seeListInput.click();
+// });
 
-categoryButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const category = button.getAttribute("data-category");
-    cards.forEach((card) => {
-      if (category === "all" || card.dataset.category.includes(category)) {
-        card.classList.add("show");
-      } else {
-        card.classList.remove("show");
-      }
-    });
-    categoryButtons.forEach((btn) => {
-      btn.classList.remove("active");
-    });
-    button.classList.add("active");
-  });
-});
+// categoryButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const category = button.getAttribute("data-category");
+//     cards.forEach((card) => {
+//       if (category === "all" || card.dataset.category.includes(category)) {
+//         card.classList.add("show");
+//       } else {
+//         card.classList.remove("show");
+//       }
+//     });
+//     categoryButtons.forEach((btn) => {
+//       btn.classList.remove("active");
+//     });
+//     button.classList.add("active");
+//   });
+// });
 
 // Show all cards by default
-cards.forEach((card) => {
-  card.classList.add("show");
-});
+// cards.forEach((card) => {
+//   card.classList.add("show");
+// });
 
 // Activate the "All" button by default
-categoryButtons[0].classList.add("active");
+// categoryButtons[0].classList.add("active");
 
-const dataElement = document.getElementById("data");
-dataElement.style.opacity = 0;
-dataElement.style.marginTop = "-20px";
+// const dataElement = document.getElementById("data");
+// dataElement.style.opacity = 0;
+// dataElement.style.marginTop = "-20px";
 
-function go(event) {
-  event.preventDefault();
-  dataElement.animate(
-    {
-      opacity: [0, 1],
-      marginTop: ["-20px", 0],
-    },
-    {
-      duration: 500,
-      fill: "forwards",
-    }
-  );
-}
-const clickerButtons = document.querySelectorAll(".clicker");
-clickerButtons.forEach((button) => {
-  button.addEventListener("click", go);
-});
+// function go(event) {
+//   event.preventDefault();
+//   dataElement.animate(
+//     {
+//       opacity: [0, 1],
+//       marginTop: ["-20px", 0],
+//     },
+//     {
+//       duration: 500,
+//       fill: "forwards",
+//     }
+//   );
+// }
+// const clickerButtons = document.querySelectorAll(".clicker");
+// clickerButtons.forEach((button) => {
+//   button.addEventListener("click", go);
+// });
 
-document.addEventListener("DOMContentLoaded", go);
+// document.addEventListener("DOMContentLoaded", go);
